2f2a1642b693a992e07b683c7a892928cf941c0a
Avishek <avishekmukherje007@gmail.com>
2025-09-14 16:14:04 +0530

    Speaker Page- Bug Fix

--- src/pages/speakers.jsx
+++ src/pages/speakers.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Speakers() {
     return (
-        <div className="sujoy">Coming Soon!</div>
+        <div className="sujoy flex items-center justify-center min-h-screen text-center text-4xl md:text-6xl font-bold">Stay Tuned!</div>
     )
 }

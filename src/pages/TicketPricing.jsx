4f9af2e8452d83f2d4e59132f5f7815c16ecde3a
Nachiketa Pahari <nachiketapahari00@gmail.com>
2025-09-19 22:48:12 +0530

    Update TicketPricing.jsx

--- src/pages/TicketPricing.jsx
+++ src/pages/TicketPricing.jsx
@@ -29,7 +29,7 @@ const plans = [
     soldOut: false,
     accent: "yellow-400",
     color: "text-blue-400",
-    btnColor: "bg-yellow-400 text-black hover:bg-gray-700",
+    btnColor: "bg-yellow-400 text-black hover:bg-yellow-500",
     link: "https://pages.razorpay.com/pl_RHCahIPp1jHJLl/view", 
   },
 ];
